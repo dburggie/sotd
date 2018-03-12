28c35d866f2efa74b62c38b7bba67a7504333fd7
Duncan Burggraf <dcburggraf@gmail.com>
2018-03-12 14:56:35 -0700

    Update Work.cpp

--- source/Work.cpp
+++ source/Work.cpp
@@ -7,11 +7,42 @@ const std::string sotd::Work::title_end     ("}\n");
 const std::string sotd::Work::count_start   ("COUNT={");
 const std::string sotd::Work::count_end     ("}\n");
 const std::string sotd::Work::entries_start ("ENTRIES={\n");
-const std::string sotd::Work::entries_end   ("\n}"
+const std::string sotd::Work::entries_end   ("\n}");
 
-sotd::Work::Work();
-sotd::Work::Work(const Work &work);
-sotd::Work::Work(const std::string &input_text);
-sotd::Work::~Work();
-void sotd::Work::read(const std::string &input_text);
-const sotd::Entry & sotd::Work::getRandomEntry() const;
+sotd::Work::Work()
+{
+	title = std::string("");
+	works = std::vector<sotd::Entry>();
+}
+					     
+sotd::Work::Work(const Work &work)
+{
+	title = std::string(work.title);
+	works = std::vector<sotd::Entry>(work.works);
+}
+
+sotd::Work::Work(const std::string &input_text)
+{
+	title = std::string("");
+	works = std::vector<sotd::Entry>();
+	read(input_text);
+}
+
+sotd::Work::~Work() { }
+
+void sotd::Work::read(const std::string &input_text)
+{
+	title = extract(input_text, sotd::Work::title_start, sotd::Work::title_end);
+	std::vector<std::string> entry_text_vector = sotd::split(input_text, sotd::Entry::title_start, sotd::Entry::title_start);
+	entry_text_vector.erase(0);
+	std::vector<std::string>::iterator i;
+	for (i = entry_text_vector.begin(); i != entry_text_vector.end(); i++)
+	{
+		works.push_back(sotd::Entry(entry_text_vector[i]));
+	}
+}
+
+const sotd::Entry & sotd::Work::getRandomEntry() const
+{
+	return works[0]; //chosen by random dice roll, guaranteed to be random
+}

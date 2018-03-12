568e4c8f5e17b80a27be7fa90abee5a294f929dd
Duncan Burggraf <dcburggraf@gmail.com>
2018-03-12 12:55:30 -0700

    Update sotd.h

--- source/sotd.h
+++ source/sotd.h
@@ -6,63 +6,66 @@
 
 namespace sotd {
 	class Entry {
-		private:
-			std::string blurb;
-			std::string length;
-			std::string text;
+	private:
+		std::string blurb;
+		std::string length;
+		std::string text;
 
-		public:
-			static const std::string blurb_start;
-			static const std::string blurb_end;
-			static const std::string length_start;
-			static const std::string length_end;
-			static const std::string text_start;
-			static const std::string text_end;
+	public:
+		static const std::string blurb_start;
+		static const std::string blurb_end;
+		static const std::string length_start;
+		static const std::string length_end;
+		static const std::string text_start;
+		static const std::string text_end;
 
-			Entry();
-			Entry(const Entry & entry);
-			Entry(std::string input_text);
-			void read(std::string input_text);
-			std::string toString();
-	}
+		Entry();
+		Entry(const Entry & entry);
+		Entry(std::string input_text);
+		~Entry();
+		void read(std::string input_text);
+		std::string toString();
+	};
 
 	class Work {
-		private:
-			std::string title;
-			std::vector<Entry> entries;
-		public:
-			static const std::string title_start;
-			static const std::string title_end;
-			static const std::string count_start;
-			static const std::string count_end;
-			static const std::string entries_start;
-			static const std::string entries_end;
+	private:
+		std::string title;
+		std::vector<Entry> entries;
+	public:
+		static const std::string title_start;
+		static const std::string title_end;
+		static const std::string count_start;
+		static const std::string count_end;
+		static const std::string entries_start;
+		static const std::string entries_end;
 
-			Work();
-			Work(const Work & work);
-			Work(std::string input_text);
-			void read(std::string input_text);
-			Entry getRandomEntry();
-	}
+		Work();
+		Work(const Work & work);
+		Work(std::string input_text);
+		~Work();
+		void read(std::string input_text);
+		Entry getRandomEntry();
+	};
 
 	class Data {
-		private:
-			std::string author;
-			std::vector<Work> works;
-		public:
-			static const std::string author_start;
-			static const std::string author_end;
-			static const std::string count_start;
-			static const std::string count_end;
-			static const std::string works_start;
-			static const std::string works_end;
+	private:
+		std::string author;
+		std::vector<Work> works;
+	public:
+		static const std::string author_start;
+		static const std::string author_end;
+		static const std::string count_start;
+		static const std::string count_end;
+		static const std::string works_start;
+		static const std::string works_end;
 
-			Data();
-			Data(const Data & data);
-			Data(std::string filename);
-			void read(std::string filename);
-			Work getRandomWork();
-	}
-}
+		Data();
+		Data(const Data & data);
+		Data(std::string filename);
+		~Data();
+		void read(std::string filename);
+		Work getRandomWork();
+	};
+};
 
 #endif

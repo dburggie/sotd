37e57a92d49fa45308e5d955259827ebca87931b
Duncan Burggraf <dcburggraf@gmail.com>
2018-03-12 14:25:27 -0700

    Update Entry.cpp

--- source/Entry.cpp
+++ source/Entry.cpp
@@ -16,35 +16,6 @@ const std::string sotd::Entry::length_end   ("}\n")
 const std::string sotd::Entry::text_start   ("TEXT={\n");
 const std::string sotd::Entry::text_end     ("\n}");
 
-
-std::string sotd::extract(const std::string & strobj, const std::string & start, const std::string & end) {
-	std::size_t i = 0, j = 0;
-	i = strobj.find(start);
-	if (i == std::string::npos) return std::string("");
-	j = strobj.find(start,i);
-	if (j == std::string::npos) return strobj.substring(i,strobj.length() - i);
-	return strobj.substr(i,j - i);
-}
-
-std::vector<std::string> sotd::split(const std::string & str, const std::string & sub) {
-	std::vector<std::string> v();
-	std::size_t i = 0, j = str.find(sub), l = sub.length();
-
-	v.push_back(str.substr(i,j - i));
-
-	while (j != std::string::npos)
-	{
-		i = j + l;
-		j = str.find(sub,i);
-		v.push_back(str.substr(i,j - i));
-	}
-
-	return v;
-}
-
-	
-	
-	
 sotd::Entry() {
 	blurb("");
 	length("");

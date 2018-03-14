ab49f95e869cd3864a74fd03d698886f605d42f9
Duncan Burggraf <dcburggraf@gmail.com>
2018-03-13 22:43:46 -0700

    Update main.cpp
    removed useless lines, added likely-unnecessary comments

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream> //std::cout
 #include <string>   //std::string
-#include <sotd.h>   //sotd::Data,Work,Entry
+#include <sotd.h>   //sotd::Data,Work,Entry,readfile
 
 int main(void)
 {
+	//read data file
 	std::string data_text = sotd::readfile(DATA_PATH);
 	if (0 == data_text.length())
 	{
 		data_text = sotd::readfile(DATA_FALLBACK);
 	}
 
+	//parse data
 	sotd::Data data(data_text);
-	sotd::Work work = data.getRandomWork();
-	sotd::Entry entry = work.getRandomEntry();
+	
+	//print a random entry
 	std::cout << data.getRandomWork().getRandomEntry().toString();
 
 	return 0;
